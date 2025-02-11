e8e6d0008480cdefcc7a711f8fdc8c78d517bed3
reribeir <rerial68@gmail.com>
2025-02-11 20:01:00 -0300

    .

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: reribeir <reribeir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 08:09:40 by reribeir          #+#    #+#             */
-/*   Updated: 2025/02/10 15:22:52 by reribeir         ###   ########.fr       */
+/*   Updated: 2025/02/11 20:00:18 by reribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,63 @@
 
 int	main(int ac, char **av, char *envp)
 {
-	int	fd;
 	pid_t	pid;
-	pid_t	pipe1[2];
+	int	pipefd[2];
 
 	if(ac != 5)
-		return(ft_printf("Error"));
+		ft_putendl_fd("Wrong number of arguments", 2);
+	if(pipe(pipefd) == -1)
+		ft_putendl_fd("Error while pipping", 2);
 	pid = fork();
 	if (pid < 0)
-		ft_printf("Error while forking");
+		ft_putendl_fd("Error while forking", 2);
 	if (pid == 0)
-		c_process(*av[2], &pipe, envp);
-	
+		c_process(**av, pipefd[0], envp);
+	waitpid(pid, 0, NULL);
+	p_process();
 	return(0);
 }
 
+char	*path_finder(char **envp, char *av)
+{
+	char	**path;
+	int		i;
+
+	i = 0;
+	while (!(ft_strncmp(*envp[i], "PATH=", 5)))
+		i++;
+	path = ft_split(*envp[i] + 5, ":");
+	i = 0;
+	while(path[i])
+	{
+		char	*arg;
+		char	*arg_path;
+
+		arg = ft_strjoin(path[i], "/");
+		arg_path = ft_strjoin(arg, av[2]);
+		if (access(arg_path, F_OK));
+		{
+			//dar free na matriz path
+			free(arg);
+			return (arg_path);
+		}
+		free(arg);
+		free(arg_path);
+		i++;
+	}
+}
+
+void	c_process(char **av, int pipefd[2], char **envp)
+{
+	int	fd;
+	char	*path;
+
+	fd = open(av[1], O_RDONLY);
+	close(pipefd[0]);
+	if (dup2(pipefd[1], STDOUT_FILENO) == -1 || dup2(fd, STDIN_FILENO) == -1)
+		ft_putendl_fd("Error on dup2", 2);
+	path = path_finder(envp, av[2]);
+	//criar comando pro exec
+	//o comando tem que ser tratado como matriz
+
+}
